52a45c5ed0c6c75e010f764ffd5d1973e4833154
lctuan0807 <tuan.le@readytech.io>
2022-03-08 21:07:11 +0700

    update

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require_relative './lib/model/item'
 require_relative './lib/model/product'
 require_relative './lib/services/exports/receipt_exporter'
 
-file_pairs = [
+files = [
   {
     input: 'input_data/input1.csv',
     output: 'results/output1.csv',
@@ -19,16 +19,16 @@ file_pairs = [
   }
 ]
 
-file_pairs.each do |file_pair|
+files.each do |file|
   receipt = Receipt.new
-  file = File.read(file_pair[:input])
-  input_data_arr =  CSV.parse(file, headers: true, header_converters: :symbol).map(&:to_h)
+  input_file = File.read(file[:input])
+  input_data_arr =  CSV.parse(input_file, headers: true, header_converters: :symbol).map(&:to_h)
   input_data_arr.each do |data|
     product = Product.new(name: data[:product], price: data[:price])
     item = Item.new(product: product, quantity: data[:quantity])
     receipt.add_item(item)
   end
-  receipt_exporter = Exports::ReceiptExporter.new(output_path: file_pair[:output])
+  receipt_exporter = Exports::ReceiptExporter.new(output_path: file[:output])
   receipt_exporter.receipt = receipt
   receipt_exporter.perform
 end
